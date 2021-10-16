4eb557d1417400d880865bac83c3b8dd84272f44
Albert Hovakimyan <HovakimyanAB@gmail.com>
2021-10-17 02:24:48 +0400

    sintax

--- xo3x3x3.py
+++ xo3x3x3.py
@@ -1,10 +1,12 @@
 import numpy as np
 from calculation import checkWinner
 # menak es parametr@ talises qashvumes mi kom
-level = 4
+level = 3
+
 
 def makeCube(level):
-    return  np.zeros((level, level, level))
+    return np.zeros((level, level, level))
+
 
 cube = makeCube(level)
 
@@ -15,15 +17,17 @@ O = level + X
 # cube[2][1][0] = 1
 # cube[2][2][0] = 1
 
-# cube[0][2][0] = 1
-# cube[1][1][1] = 1
-# cube[2][0][2] = 1
+cube[0][2][0] = 1
+cube[1][1][1] = 1
+cube[2][0][2] = 1
 
-cube[0,0,0] = 1
-cube[1,1,3] = 1
-cube[2,2,2] = 1
+# cube[0,0,0] = O
+# cube[1,1,1] = O
+# cube[2,2,2] = O
+# cube[3,3,3] = O
+
+
 
 
 print(cube)
 print(checkWinner(cube, X, O, level))
-
